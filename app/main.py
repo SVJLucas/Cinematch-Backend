e44cb0b23fa5d037cd5da8769e70392a971ea48f
SVJLucas <lucas.jose.veloso.de.souza@gmail.com>
2023-07-17 04:06:41 +0200

    Updating and Commenting main

--- app/main.py
+++ app/main.py
@@ -1,20 +1,21 @@
 from fastapi import FastAPI
-from routers import genres,movies,movies_genres
+from routers import genres, movies, movies_genres
 from database.database import connect_to_database
 from dotenv import load_dotenv
 
-
-# Carregar as variáveis do arquivo .env
+# Load environment variables from a .env file
 load_dotenv()
-app = FastAPI()
+
+# Initialize the FastAPI application
+app = FastAPI(title='CinematchAPI')
+
+# Connect to the Firebase database
 connect_to_database()
 
-
-app.include_router(genres.router)
-app.include_router(movies.router)
-app.include_router(movies_genres.router)
-
-
-
-
-
+# Include routers, assigning tags to categorize the routes in the API docs
+# The genre-related routes
+app.include_router(genres.router,tags=['Genres'])
+# The movie-related routes
+app.include_router(movies.router,tags=['Movies'])
+# The routes related to relationships between movies and genres
+app.include_router(movies_genres.router,tags=['Movies and Genres Relations'])
